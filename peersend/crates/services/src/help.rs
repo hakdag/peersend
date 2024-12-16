651e5869167839e11e9c4bd1133ec52e55489f15
Hakan <hakanakdag46@gmail.com>
2024-12-16 10:54:41 +0300

    PS-14: Added mock help service

--- peersend/crates/services/src/help.rs
+++ peersend/crates/services/src/help.rs
@@ -5,6 +5,16 @@ pub struct HelpService {}
 
 impl HelpService {
     pub fn run() -> Result<String, Error> {
-        Result::Ok("No help is supported at the moment.".to_string())
+        let mut parts = vec!["peersend [command] [arguments]"];
+        parts.push("Commands and arguments:");
+        parts.push("help");
+        parts.push("version");
+        parts.push("create-user [username] [password] [email]");
+        parts.push("login [username] [password]");
+        parts.push("register-device [username] [devicename]");
+        parts.push("send [filepath] [source device name] [target device name]");
+
+        let output = parts.join("\n");
+        Result::Ok(output)
     }
 }
